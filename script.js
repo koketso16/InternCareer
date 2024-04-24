2a8b8d3e434e5f48ddb0742d680b9bfe3a567098
Koketjo Mothapo <99620457+koketso16@users.noreply.github.com>
2024-04-24 22:15:12 +0200

    Update CV file

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
   downloadBtn.addEventListener("click", function() {
     const link = document.createElement("a");
-    link.href = "cv.pdf";
-    link.download = "cv.pdf";
+    link.href = "KoketjoMothapoCV.pdf";
+    link.download = "KoketjoMothapoCV.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -26,36 +26,4 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-//document.addEventListener("DOMContentLoaded", function() {
-//      const contactForm = document.getElementById("contactForm");
-//
-//      contactForm.addEventListener("submit", function(event) {
-//        event.preventDefault(); // Prevent form submission
-//        var formData = new FormData(contactForm);
-//
-//        fetch("https://formspree.io/f/xwkgarbz", {
-//          method: "POST",
-//          body: formData
-//        })
-//        .then(response => response.text())
-//        .then(result => {
-//          // Display result
-//          var messageSentDiv = document.createElement("div");
-//          messageSentDiv.textContent = result;
-//          messageSentDiv.style.color = result === "Form submitted. Thank you!" ? "green" : "red";
-//          contactForm.appendChild(messageSentDiv);
-//
-//          // Clear the form
-//          contactForm.reset();
-//
-//          // Redirect to home page after 3 seconds
-//          setTimeout(function() {
-//            window.location.href = "home.html";
-//          }, 3000);
-//        })
-//        .catch(error => {
-//          console.error("Error:", error);
-//        });
-//      });
-//    });
 
